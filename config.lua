2f1d7e1ce20a7cc187c3025f82721ce86742843b
Matthew Brooks <mb964033@gmail.com>
2023-12-12 21:23:19 +0000

    update santoku deps

--- config.lua
+++ config.lua
@@ -1,20 +1,20 @@
 local env = {
 
   name = "santoku-html",
-  version = "0.0.1-1",
+  version = "0.0.2-1",
   variable_prefix = "TK_HTML",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
+    "santoku >= 0.0.145-1",
     "lpeg >= 1.1.0-1",
   },
 
   test_dependencies = {
+    "santoku-test >= 0.0.2-1",
     "luassert >= 1.9.0-1",
-    "luaposix >= 36.2.1-1",
-    "santoku >= 0.0.143-1",
     "luacheck >= 1.1.0-1",
     "luacov >= 0.15.0-1",
   },
