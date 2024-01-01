470648442bff9778632ac5fecfe6af0a78795f71
Matthew Brooks <mb964033@gmail.com>
2024-01-01 17:55:22 +0000

    migrate to toku make

--- make.lua
+++ make.lua
@@ -1,22 +1,24 @@
 local env = {
 
   name = "santoku-html",
-  version = "0.0.2-1",
+  version = "0.0.3-1",
   variable_prefix = "TK_HTML",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.145-1",
+    "santoku >= 0.0.158-1",
     "lpeg >= 1.1.0-1",
   },
 
-  test_dependencies = {
-    "santoku-test >= 0.0.2-1",
-    "luassert >= 1.9.0-1",
-    "luacheck >= 1.1.0-1",
-    "luacov >= 0.15.0-1",
+  test = {
+    dependencies = {
+      "santoku-test >= 0.0.6-1",
+      "luassert >= 1.9.0-1",
+      "luacheck >= 1.1.0-1",
+      "luacov >= 0.15.0-1",
+    }
   },
 
 }
@@ -26,5 +28,6 @@ env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
+  type = "lib",
   env = env,
 }
